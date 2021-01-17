2eb50ac2ba89106ff6dfba33858d725f739d51e1
poommin <phoomja@hotmail.com>
2021-01-17 10:09:17 +0700

    add new file access.py add new file add_list.py

---
+++ lists/add_list.py
@@ -0,0 +1,9 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+
+print(thislist)  # ['apple', 'banana', 'cherry', 'orange']
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)  # ['apple', 'orange', 'banana', 'cherry']
