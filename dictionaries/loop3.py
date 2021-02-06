8c6348652fcc1e3cabeefd405d7726beaf6d66df
poommin <phoomja@hotmail.com>
2021-02-06 11:13:15 +0700

    add new file dic.py

---
+++ dictionaries/loop3.py
@@ -0,0 +1,5 @@
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+
+
+for Value in thisdict.values():
+    print(Value)
---
+++ dictionaries/loop4.py
@@ -0,0 +1,5 @@
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+
+
+for Key, Value in thisdict.items():
+    print(Key, Value)
