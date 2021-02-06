bfa0a9ef3ed584a21dbf02c69ca77166a42b9e22
poommin <phoomja@hotmail.com>
2021-02-06 22:01:41 +0700

    in class

---
+++ tuples/accers.py
@@ -0,0 +1,12 @@
+thistuple = ("apple", "banana", "cherry")
+
+print(thistuple[0])
+print(thistuple[1])
+print(thistuple[2])
+
+print(thistuple[-1])
+print(thistuple[-2])
+print(thistuple[-3])
+
+
+print(thistuple[1:])
